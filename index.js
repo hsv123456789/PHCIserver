44aaf5caa89e05c770cab2d6e0556fb1fa1d1b9e
hsv123456789 <harisrevatcha@gmail.com>
2022-10-21 13:03:33 +0530

    Added basic backend functionality

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const port = 3000;
 const express = require("express");
 const app = express();
+const url =
+  "mongodb+srv://hsv123456789:vaishnavee@cluster0.yzu2ier.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+const { userModel } = require("./models/user");
 
-app
-  .get("/", (request, response) => {
-    response.send("<h1>The sample web page is</h1>");
-  })
-  .listen(port, () => {
-    console.log(`The application is starting at the port ${port}`);
+app.get("/", async (request, response) => {
+  const user = await userModel.create({
+    name: "admin",
+    password: "admin",
   });
+  response.json(user);
+});
+mongoose.connect(url, (error) => {
+  console.log("The connection is ", error);
+});
+
+app.listen(port, () => {
+  console.log(`The application is starting at the port ${port}`);
+});
